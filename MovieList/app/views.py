29104a8f4467440f9025d50eb1d4e419eaf31ed2
Christian Pustianu <sc20ccp@leeds.ac.uk>
2021-11-26 22:16:31 +0000

    Set up initial repo

---
+++ MovieList/app/views.py
@@ -0,0 +1,40 @@
+from flask import render_template, flash, request, redirect
+from flask.wrappers import Request
+from app import app, db
+from app.models import Assessment
+from .forms import AssessmentForm, IdTypeForm
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	home={'description':'Welcome to MovieList!'}
+	return render_template('home.html', title='Home', home=home)
+
+# @app.route('/all_assessments', methods=['GET', 'POST'])
+# def all_assessments():
+#     # form1,2 are used to retrieve the id of an assessment and the type of button when a button is pushed
+#     form1 = IdTypeForm()
+#     form2 = IdTypeForm()
+#     assessments = Assessment.query
+#     # rows is used to check if there are assessments created
+#     rows = assessments.count()
+#     if request.method == "POST":
+#         assessment_to_update = Assessment.query.get(form1.id.data)
+#         # check which button was pressed and perform the corresponding action
+#         if form2.type.data == "uncompleted":
+#             assessment_to_update.status = 1
+#         elif form2.type.data == "completed":
+#             assessment_to_update.status = 0
+#         elif form2.type.data == "delete":
+#             db.session.delete(assessment_to_update)
+#         try:
+#             db.session.commit()
+#             return redirect('/all_assessments')
+#         except:
+#             return redirect('/all_assessments')
+#     else:
+#         return render_template('all_assessments.html',
+#                                 title='All Assessments',
+#                                 assessments=assessments,
+#                                 form1=form1,
+#                                 form2=form2,
+#                                 rows=rows)
